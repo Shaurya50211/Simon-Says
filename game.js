f69a02373e0a470c23c981869bb8d0d2b67be107
shaurya50211 <shaurya50211@gmail.com>
2023-03-19 10:03:15 -0400

    added bg music

--- game.js
+++ game.js
@@ -3,10 +3,11 @@ var randomChosenColor = ""
 var gamePattern = []
 var userClickedPattern = []
 var level = 0
+var bgMusic = []
 var started = false;
 
 function nextSequence() {
-    let randomNumber = Math.floor(Math.random()*4)
+    let randomNumber = Math.floor(Math.random() * 4)
     console.log(randomNumber)
     randomChosenColor = buttonColours[randomNumber]
     gamePattern.push(randomChosenColor)
@@ -20,7 +21,7 @@ function nextSequence() {
 }
 
 function playSound(name) {
-    var colorSound = new Audio("sounds/"+name+".mp3")
+    var colorSound = new Audio("sounds/" + name + ".mp3")
     colorSound.play()
 }
 
@@ -31,11 +32,34 @@ function animatePress(currentColour) {
     }, 100);
 }
 
+function playBg(index) {
+    bgMusic[index].volume = 0.5
+    bgMusic[index].loop = true
+    bgMusic[index].play()
+}
+
 $('body').on('keypress', function (e) {
-    if (started == false) {
+    if (started == false && e.keyCode == 13) {
+        bgMusic[0] = new Audio("sounds/game-music.mp3")
+        bgMusic[1] = new Audio("sounds/bg-music2.mp3")
+        playBg(0)
+        $('#music-int').css("display", "inline")
         nextSequence()
         $('h1').text("Level " + level)
         started = true;
+    }
+
+    if (e.key == 'm') {
+        bgMusic[0].pause();
+        bgMusic[0].currentTime = 0;
+        bgMusic[1].pause();
+        bgMusic[1].currentTime = 0;
+    }
+
+    if(e.key == 's') {
+        bgMusic[0].pause();
+        bgMusic[0].currentTime = 0;
+        playBg(1);
     }
 });
 
@@ -60,17 +84,17 @@ function checkAnswer(currentLevel) {
 }
 
 function startOver() {
-    $('h1').text("Uh-oh! Press Any Key To Restart!")
+    $('h1').text("Uh-oh! Press Enter To Restart!")
     started = false
     gamePattern = []
     level = 0
 }
 
-$('.btn').on("click", function() {
+$('.btn').on("click", function () {
     var userChosenColour = $(this).attr('id');
     animatePress(userChosenColour)
     playSound(userChosenColour)
     userClickedPattern.push(userChosenColour)
     console.log(userClickedPattern)
-    checkAnswer(userClickedPattern.length-1);
+    checkAnswer(userClickedPattern.length - 1);
 })
